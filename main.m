810f0b8d227d4fe798e58cfedf3ff74cae3536dd
Flannery Jefferson <flannj@gmail.com>
2019-04-08 16:51:58 -0400

    basic tiny app running

--- main.m
+++ main.m
@@ -1,12 +1,44 @@
-// gcc -framework Foundation -x objective-c ./main.m ; ./a.out ; echo $?
+// gcc -framework Cocoa -x objective-c -o MinimalistCocoaApp ./main.m
 
 #import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
-int main(int argc, char** argv)
-{
-  NSLog(@"Testing");
-  id pool = [NSAutoreleasePool new];
-  NSLog(@"%@", [ [NSProcessInfo processInfo] arguments]);
-  [pool drain];
-  return 0;
+int main() {
+  [NSAutoreleasePool new];
+  [NSApplication sharedApplication];
+  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+
+  id appName = [[NSProcessInfo processInfo] processName];
+
+  // create menubar
+  id menubar = [[NSMenu new] autorelease];
+
+  // populate app menu
+  /* id menuItem = [[NSMenuItem new] autorelease];
+  id submenu = [[NSMenu new] autorelease];
+  id quitMenuItem = [[[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
+  [submenu addItem:quitMenuItem];
+  [menuItem setSubmenu:submenu];
+  [menubar addItem:menuItem];
+
+  // populate menubar: edit menu
+  menuItem = [[NSMenuItem new] autorelease];
+  submenu = [[NSMenu new] autorelease];
+  id cutMenuItem = [[[NSMenuItem alloc] initWithTitle:@"Cut" action:@selector(cut:) keyEquivalent:@"x"] autorelease];
+  [submenu addItem:cutMenuItem];
+  [menuItem setSubmenu:submenu];
+  [menubar addItem:menuItem]; */
+
+  [NSApp setMainMenu:menubar];
+
+  id window = [[[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 200, 200)
+  styleMask:NSWindowStyleMaskTitled backing:NSBackingStoreBuffered defer:NO]
+  autorelease];
+  [window cascadeTopLeftFromPoint:NSMakePoint(20,20)];
+  [window setTitle:appName];
+  [window makeKeyAndOrderFront:nil];
+  [NSApp activateIgnoringOtherApps:YES];
+  [NSApp run];
 }
+
+
